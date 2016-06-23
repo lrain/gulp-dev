2a4ce7d247abadec675bb0619ab1561857ee0ecc
lrain <leiyu86@gmail.com>
2016-06-23 18:21:34 +0800

    add handerbars

--- script/page_webpack/index.js
+++ script/page_webpack/index.js
@@ -1,1 +1,8 @@
 require('../module/index.js')();
+var context = {
+   title: "My New Post",
+   body: "This is my first post!"
+};
+var Handlebars = require('../template/entry.handlebars');
+var tmpl = Handlebars(context);
+console.log(tmpl);
